30b3a2767c0996b5a015cf0b60d2ca76b7ccb530
Zachary Wong <zachary.j.wong.23@dartmouth.edu>
2022-09-19 22:01:07 -0400

    rename done

--- Particle_Future/VCTool/Install.py
+++
@@ -1,9 +0,0 @@
-import sys
-import subprocess
-class Install:
-
-    # Install all packages needed to webscrape
-    def installPackages(self):
-        packages = ["flask"]
-        for package in packages:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", package])  # import data libraries
