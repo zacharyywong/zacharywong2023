f687cd61c3b746e5e19ae96ea9ccc37039940d72
Zachary Wong <zachary.j.wong.23@dartmouth.edu>
2022-11-03 18:45:56 -0400

    up to question 4

--- CS61_3/lab3.py
+++ CS61_3/lab3.py
@@ -8,16 +8,45 @@ def get_database():
    client = pymongo.MongoClient("mongodb+srv://zacharyywong:" + urllib.parse.quote("3Cs@CodeRed") + "@lab3.htmdfxa.mongodb.net/?retryWrites=true&w=majority")
    db = client.lab3 
    return db
-  
+
+#Assumes every document id equals city zip code and is unique 
+def question2(db):
+    total = db.zipcodes.count_documents({})
+    # total = 0
+    # for document in cursor:
+    #      total += 1
+    print("Question 2: " + str(total))
+    return total
+
+def question3(db):
+    total = db.zipcodes.count_documents({"state":{"$in":['CT', 'RI', 'MA', 'VT', 'NH', 'ME']}})
+    print("Question 3: " + str(total))
+    return total
+
+
+def question4(db):
+    cursor = db.zipcodes.aggregate([
+        
+        {
+            "$group": {"_id": {'state': "$state", 'population': '$population'},'total': {"$sum": '$pop'}}
+        },
+
+        {
+             "$match": {"_id":{'state': 'RI'}}
+         }
+       
+    ])
+    for document in cursor:
+        print("Question 4: " + str(document))
+
+
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":   
-
-    
     # Get the database
     db = get_database()
+    answer2 = question2(db)
+    answer3 = question3(db)
+    answer4 = question4(db)
 
 
     
-    # cursor = db.zipcodes.find({"_id": "01001"})
-    # for document in cursor:
-    #     print(document)
